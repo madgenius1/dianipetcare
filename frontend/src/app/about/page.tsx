e3e4f17a3d7d48e29ebb9dd14b7f1e34611f20d2
ME <nickjuma@outlook.com>
2025-10-02 01:09:36 +0300

    frontend/src/app/about/         frontend/src/app/booking/         frontend/src/app/components/         frontend/src/app/contact/         frontend/src/app/faqs/         frontend/src/app/services/         package-lock.json         package.json         pnpm-lock.yaml         pnpm-workspace.yaml

---
+++ frontend/src/app/about/page.tsx
@@ -0,0 +1,15 @@
+
+
+
+
+export default function Page() {
+    return(
+        <main className="min-h-screen flex items-center justify-center">
+            <div className="text-center">
+                <h3 className="text-3xl font-medium text-gray-950">
+                    About
+                </h3>
+            </div>
+        </main>
+    )
+}
