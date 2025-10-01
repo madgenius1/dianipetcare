e14129cca865b48c123a3ffeee362ad820a774f8
ME <nickjuma@outlook.com>
2025-10-02 01:52:20 +0300

    Modified frontend Footer, Navbar, layout, added global.d.ts

--- frontend/src/app/components/common/Footer.tsx
+++ frontend/src/app/components/common/Footer.tsx
@@ -1,10 +1,14 @@
 
 
 export default function Footer() {
-    return(
+    return (
         <footer className="max-w-screen-xl mx-auto">
             <div className="lg:py-20 md:py-16 py-12 lg:px-8 md:px-6 px-4">
-                
+                <div className="flex items-center justify-center text-center">
+                    <h3 className="text-3xl font-medium text-gray-950">
+                        Footer
+                    </h3>
+                </div>
             </div>
         </footer>
     )
--- frontend/src/app/components/common/Navbar.tsx
+++ frontend/src/app/components/common/Navbar.tsx
@@ -3,19 +3,21 @@ import Link from "next/link"
 
 export default function Navbar() {
     return (
-        <nav className="sticky top-0 max-w-screen-xl mx-auto z-50 lg:px-8 md:px-6 px-4 py-6 bg-transparent shadow-lg">
-            <div className="flex items-center justify-start">
-                <h3 className="text-gray-950 font-medium">Diani Pet Care</h3>
-            </div>
-            <div className="flex items-center justify-center">
-                <div className="flex justify-between space-x-3">
-                    <Link href="/services" className="text-gray-800 font-medium hover:underline">Services</Link>
-                    <Link href="/booking" className="text-gray-800 font-medium hover:underline">Booking</Link>
-                    <Link href="/about" className="text-gray-800 font-medium hover:underline">About</Link>
+        <nav className="lg:px-8 md:px-6 px-4 py-5 max-w-screen-xl mx-auto sticky top-0 z-50 bg-white/80 backdrop-blur-sm shadow-md">
+            <div className="flex items-center justify-between ">
+                <div className="flex justify-start">
+                    <Link href="/" className="text-gray-950 font-medium">Diani Pet Care</Link>
                 </div>
-            </div>
-            <div className="flex justify-end">
-                <Link href="/contact" className="hover:text-gray-800 hover:bg-gray-100 rounded-md px-6 py-3 font-medium hover:underline">About</Link>
+                <div className="flex justify-center">
+                    <div className="flex justify-between space-x-6">
+                        <Link href="/services" className="text-gray-800 font-medium hover:underline">Services</Link>
+                        <Link href="/booking" className="text-gray-800 font-medium hover:underline">Booking</Link>
+                        <Link href="/about" className="text-gray-800 font-medium hover:underline">About</Link>
+                    </div>
+                </div>
+                <div className="flex justify-end">
+                    <Link href="/contact" className="hover:text-gray-800 hover:bg-gray-100 rounded-md px-6 py-3 font-medium hover:underline">About</Link>
+                </div>
             </div>
         </nav>
     )
