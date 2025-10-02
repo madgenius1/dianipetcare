861b12e64d63e7188eb544812f4a3e3600b82ee5
ME <nickjuma@outlook.com>
2025-10-02 15:48:42 +0300

    modified:   frontend/package.json         modified:   frontend/src/app/components/common/Footer.tsx         modified:   frontend/src/app/components/landing/HowItWorks.tsx         modified:   pnpm-lock.yaml

--- frontend/src/app/components/common/Footer.tsx
+++ frontend/src/app/components/common/Footer.tsx
@@ -1,13 +1,122 @@
 
+import Link from "next/link"
+import Image from "next/image"
+import { FaFacebook, FaWhatsapp, FaLinkedin, FaInstagram, FaTiktok } from "react-icons/fa";
+import { RiTwitterXFill } from "react-icons/ri";
+
+
+const links = [
+    { url: "mailto:info@foom.app", title: "info@foom.app" },
+    { url: "tel:+254712345678", title: "+254 712 345 678" },
+    { url: "tel:+254712345678", title: "+254 712 345 678" },
+]
+const socials = [
+    { icon: <FaFacebook size={24} className="dark:text-gray-900 text-white" />, url: "https://www.facebook.com" },
+    { icon: <FaWhatsapp size={24} className="dark:text-gray-900 text-white" />, url: "https://www.whatsapp.com" },
+    { icon: <FaInstagram size={24} className="dark:text-gray-900 text-white" />, url: "https://www.instagram.com" },
+    { icon: <FaTiktok size={24} className="dark:text-gray-900 text-white" />, url: "https://www.tiktok.com" },
+    { icon: <RiTwitterXFill size={24} className="dark:text-gray-900 text-white" />, url: "https://www.x.com" }
+]
+
+const details = [
+    {
+        id: "Company",
+        links: [
+            { textOne: "Services", linkOne: "/services" },
+            { textOne: "Booking", linkOne: "/booking" },
+            { textOne: "About Us", linkOne: "/about" },
+            { textOne: "Subscribe", linkOne: "/subscribe" },
+        ]
+    },
+    {
+        id: "Support",
+        links: [
+            { textOne: "How It Works", linkOne: "#working" },
+            { textOne: "FAQs", linkOne: "/faqs" },
+            { textOne: "Privacy Policy", linkOne: "/privacy" },
+            { textOne: "Terms of Use", linkOne: "/terms" },
+        ]
+    }
+];
+
 
 export default function Footer() {
+    const currentYear = new Date().getFullYear();
+
     return (
-        <footer className="max-w-screen-xl mx-auto">
-            <div className="lg:py-20 md:py-16 py-12 lg:px-8 md:px-6 px-4">
-                <div className="flex items-center justify-center text-center">
-                    <h3 className="text-3xl font-medium text-gray-950">
-                        Footer
-                    </h3>
+        <footer className=" dark:bg-gray-100 bg-gray-800">
+            <div className="lg:px-8 md:px-6 px-4 lg:py-20 md:py-16 py-12 max-w-screen-xl mx-auto">
+                <div className="flex lg:flex-row flex-col lg:justify-between gap-4 p-4">
+                    {/* Left Content */}
+                    <div className="lg:w-1/3 w-full flex flex-col gap-4 py-4">
+                        <div className="flex flex-col">
+                            <Link href="/" className="-m-1.5 p-2">
+                                <Image
+                                    src="/foom.svg"
+                                    alt="Diani Pet Care logo"
+                                    width={140}
+                                    height={70}
+                                />
+                            </Link>
+                            <p className="text-gray-900 font-semibold text-md">
+                                Diani Pet Care. <br />
+                                Your reliable pet care partner for dog walking, grooming, and boarding.
+                            </p>
+                        </div>
+                        {/* <div className="lg:py-8 md:py-6 py-4 flex flex-col space-y-2">
+                            {links.map((link, index) =>
+                            (
+                                <Link key={index} href={link.url} className="text-sm lg:text-md hover:underline font-medium text-white dark:text-gray-900">
+                                    {link.title}
+                                </Link>
+                            ))}
+                        </div> */}
+                        <p className="py-4 text-sm lg:text-md font-medium text-white dark:text-gray-900">
+                            Diani Mall, Beach Road, Ukunda
+                        </p>
+                        <div className="py-6 flex flex-row space-x-4 ">
+                            {socials.map((social, index) =>
+                            (
+                                <Link key={index} href={social.url} className="rounded-full">
+                                    {social.icon}
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                    {/* Right Content */}
+                    <div className="lg:w-2/3 w-full lg:flex lg:justify-end lg:px-12 md:px-8">
+                        <div className="grid grid-cols-2 lg:grid-cols-2 gap-6 md:grid-cols-2">
+                            {details.map((section, index) => (
+                                <div key={index} className="flex flex-col space-y-2">
+                                    <h3 className="text-md font-semibold text-white dark:text-gray-950">
+                                        {section.id}
+                                    </h3>
+                                    <ul className="space-y-4">
+                                        {section.links.map((link, linkIndex) => (
+                                            <li key={linkIndex}>
+                                                <Link href={link.linkOne} className="text-gray-400 hover:text-white dark:text-gray-800 dark:hover:text-blue-700">
+                                                    {link.textOne}
+                                                </Link>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            ))}
+                        </div>
+
+                    </div>
+                </div>
+                <hr className="border-gray-700 py-4" />
+                <div className=" flex flex-col gap-y-2">
+                    <p className=" text-sm text-center lg:text-md">
+                        <strong className="text-white dark:text-gray-900">Diani Pet Care | Copyright </strong> © {currentYear} | All Rights Reserved
+                    </p>
+                    <p className="text-center">
+                        A platform by{' '}
+                        <Link href="https://nickjuma.vercel.app/" target="_blank" rel="noopener noreferrer" className="font-semibold text-gray-950 transition-colors duration-200">
+                            Juma
+                        </Link>
+                    </p>
                 </div>
             </div>
         </footer>
--- frontend/src/app/components/landing/HowItWorks.tsx
+++ frontend/src/app/components/landing/HowItWorks.tsx
@@ -2,7 +2,7 @@
 
 export default function HowItWorks() {
     return(
-        <section className="min-h-screen">
+        <section id="working" className="min-h-screen">
             <div className="flex items-center justify-center text-center">
                 <h3 className="text-3xl font-medium text-gray-950">
                     Services SnapShot
